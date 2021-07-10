f61a2ae79df9c58a0125cba85fa3bcd47b14baec
BB2Xd <74978198+BB2Xd@users.noreply.github.com>
2021-07-11 02:29:01 +0530

    Up

--- tgfilestream/telegram.py
+++ tgfilestream/telegram.py
@@ -40,8 +40,8 @@ async def handle_message(evt: events.NewMessage.Event) -> None:
     if Config.JTU_ENABLE:
     	starky = await check_if_subbed(Config.CHANNEL_USERNAME)
     	if starky is False:
-        	await evt.reply("**I am Sorry To Say That, To Access Me You Have To Be The Member Of Our Channel To Use This Bot..!**", buttons=[[custom.Button.url("Join Channel", Config.CHANNEL_URL)]])
-        return
+        	await event.reply("**I am Sorry To Say That, To Access Me You Have To Be The Member Of Our Channel To Use This Bot..!**", buttons=[[custom.Button.url("Join Channel", Config.CHANNEL_URL)]])
+        	return
     if not evt.is_private:
         await evt.reply(group_chat_message)
         return
