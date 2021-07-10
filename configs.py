ec44f0e55ffc59b1ea0066091ec52fa6a0e2a5c6
BB2Xd <74978198+BB2Xd@users.noreply.github.com>
2021-07-11 01:49:36 +0530

    Up

--- configs.py
+++ configs.py
@@ -3,3 +3,4 @@ import os
 class Config(object):
     JTU_ENABLE = os.environ.get("JTU_ENABLE", True)
     CHANNEL_USERNAME = os.environ.get("CHANNEL_USERNAME", None)
+    CHANNEL_URL = os.environ.get("CHANNEL_URL", None)
