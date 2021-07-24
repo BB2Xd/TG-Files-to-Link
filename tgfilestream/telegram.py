3cf18d5cae363ec64484d0eb4874bc3ef8a50776
BB2Xd <74978198+BB2Xd@users.noreply.github.com>
2021-07-24 20:38:02 +0530

    UP

--- tgfilestream/telegram.py
+++ tgfilestream/telegram.py
@@ -1,6 +1,6 @@
 # tgfilestream - A Telegram bot that can stream Telegram files to users over HTTP.
 # Copyright (C) 2019 Tulir Asokan
-#
+#₹
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
